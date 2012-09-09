d4a34859a80b8e4c92c873f9de14fd0865b9d5f5
Boris Byk <boris.byk@gmail.com>
2012-09-08 21:42:14 -0700

    Add performance counters to monitor the test
    1. main loop counter - web server requests per second
    2. requests counter - memcached requests per second
    3. avg response time - web server average response time

--- src/test/java/bbyk/loadtests/MemcachedClientsLoadTest.java
+++ src/test/java/bbyk/loadtests/MemcachedClientsLoadTest.java
@@ -3,6 +3,7 @@ package bbyk.loadtests;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -10,11 +11,9 @@ import java.net.InetSocketAddress;
 import java.util.Properties;
 import java.util.Random;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Most of the implementation is internalized in one method to be easily readable.
@@ -46,14 +45,14 @@ public class MemcachedClientsLoadTest {
         };
 
         final ClientSetup[] clientSetups = ClientSetup.values();
-        for (final ClientSetup value : clientSetups) {
+        for (final ClientSetup clientSetup : clientSetups) {
             for (int i = 0; i < params.length; ) {
-                testRoundtrip(value, params[i++], params[i++], params[i++]);
+                testRoundtrip(clientSetup, params[i++], params[i++], params[i++]);
             }
         }
     }
 
-    private void testRoundtrip(final ClientSetup setup, final int docSize, final int threadCount, final int sessionLoopCount) throws Exception {
+    private void testRoundtrip(@NotNull final ClientSetup setup, final int docSize, final int threadCount, final int sessionLoopCount) throws Exception {
         System.out.printf("Setup: %s, document size: %d, threadCount: %d, sessionLoopCount: %s", setup, docSize, threadCount, sessionLoopCount);
         System.out.println();
 
@@ -70,6 +69,10 @@ public class MemcachedClientsLoadTest {
         final AtomicInteger errorCount = new AtomicInteger();
         final ConcurrentHashMap<Class, Exception> errorSet = new ConcurrentHashMap<Class, Exception>();
         final ClientFactory clientFactory = new ClientFactory(setup, localhostAddr);
+        final AtomicInteger loopCount = new AtomicInteger();
+        final AtomicInteger reqCount = new AtomicInteger();
+        final AtomicLong avgRespTime = new AtomicLong();
+        final AtomicInteger avgRespTimeBase = new AtomicInteger();
 
         // prepare workers
         for (int i = 0; i < threadCount; i++) {
@@ -80,15 +83,28 @@ public class MemcachedClientsLoadTest {
             executorService.submit(new Runnable() {
                 public void run() {
                     try {
+                        // emulated main application loop:
+                        // 1. load from memcache
+                        // 2. do some actions
+                        // 3. save to memcache
                         for (int j = 0; j < sessionLoopCount; j++) {
+                            long startMs = System.currentTimeMillis();
+
                             // read from memcache
                             final String cacheKey = "actorId:" + actorPrefix + ":" + actorId;
                             byte[] bytes = client.get(cacheKey);
+                            reqCount.incrementAndGet();
+
                             if (bytes == null)
                                 bytes = seedBuffer;
                             // modify data -- skipped
                             // write from memcache
                             client.set(cacheKey, bytes);
+                            reqCount.incrementAndGet();
+
+                            loopCount.incrementAndGet();
+                            avgRespTime.addAndGet(System.currentTimeMillis() - startMs);
+                            avgRespTimeBase.incrementAndGet();
                         }
                     } catch (Exception e) {
                         errorCount.incrementAndGet();
@@ -101,7 +117,33 @@ public class MemcachedClientsLoadTest {
             });
         }
 
-        allDone.await();
+        // poll until end and read counters
+        int lastLoopCount = loopCount.get();
+        int lastReqCount = reqCount.get();
+        long lastAvgRespTime = avgRespTime.get();
+        int lastAvgRespTimeBase = avgRespTimeBase.get();
+
+        while (!allDone.await(1, TimeUnit.SECONDS)) {
+            final int newLoopCount = loopCount.get();
+            final int newReqCount = reqCount.get();
+            final long newAvgRespTime = avgRespTime.get();
+            final int newAvgRespTimeBase = avgRespTimeBase.get();
+            final int dxRespTimeBase = newAvgRespTimeBase - lastAvgRespTimeBase;
+
+            System.out.printf("loops per sec: %d, memcached req per sec: %d, avg resp time(ms): %d",
+                    (newLoopCount - lastLoopCount),
+                    (newReqCount - lastReqCount),
+                    dxRespTimeBase == 0 ? 0 : (newAvgRespTime - lastAvgRespTime) / dxRespTimeBase);
+
+            System.out.println();
+            lastLoopCount = newLoopCount;
+            lastReqCount = newReqCount;
+
+            if (dxRespTimeBase > 0) {
+                lastAvgRespTime = newAvgRespTime;
+                lastAvgRespTimeBase = newAvgRespTimeBase;
+            }
+        }
 
         // report errors
         if (errorCount.get() > 0) {
